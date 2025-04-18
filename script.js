0518ef6aba0cb485692ea0c4b3d0646efe99c90c
nermixon <100354742+nermixon@users.noreply.github.com>
2025-04-18 08:43:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,21 +2,19 @@ const btn = document.getElementById("start-btn");
 const video = document.getElementById("vid");
 const audio = document.getElementById("sfx");
 
-// Эмулируем клик по кнопке через 0.1 секунды после загрузки страницы
 window.addEventListener("load", () => {
   setTimeout(() => {
-    // Эмуляция клика по кнопке
-    btn.click();
-  }, 100);  // Задержка 0.1 секунды, чтобы сразу эмулировать клик
+    btn.click();  // Автоматическое нажатие кнопки через 0.1 секунды
+  }, 100);  // Задержка 0.1 секунды
 });
 
 btn.addEventListener("click", async () => {
-  btn.style.display = "none"; // Скрыть кнопку
-  video.style.display = "block"; // Показать видео на весь экран
+  btn.style.display = "none";
+  video.style.display = "block";
 
   try {
-    await video.play();  // Воспроизведение видео
-    await audio.play();  // Воспроизведение звука
+    await video.play();
+    await audio.play();
   } catch (err) {
     console.error("Ошибка запуска медиа:", err);
   }
